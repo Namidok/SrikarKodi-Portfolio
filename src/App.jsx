6edb3f39d637f2a92677714958ce164acfadd0d4
Srikar Kodi <142985862+Namidok@users.noreply.github.com>
2026-06-19 16:15:21 +0200

    tracker issue is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import useScrollFade from "./hooks/useScrollFade"
+import useTracker from "./hooks/useTracker"
 import Navbar from "./components/Navbar"
 import Chatbot from "./components/Chatbot"
 import ParticleBackground from "./components/ParticleBackground"
@@ -10,6 +11,7 @@ import Contact from "./sections/Contact"
 
 export default function App() {
   useScrollFade()
+  useTracker()
 
   return (
     <div className="min-h-screen bg-[#0D0D0D]">
